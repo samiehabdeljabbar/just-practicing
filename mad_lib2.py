2eded5bf6a750d500796f16d306c0be97940ad73
Samih Abdeljabbar <49135655+samih-adam@users.noreply.github.com>
2021-10-19 15:02:28 -0500

    tryin it the way the instructor is teaching it
    I tried the first lesson bymyself in madlibs, now I would like to see how he is approaching it. It seems like new tools are being taught, but awesome that I was able to take old tools and mimic what he is teaching

---
+++ mad_lib2.py
@@ -0,0 +1,5 @@
+# I first tried it out my way, now I am seeing how the instructor will make the mad libs work!
+
+print("Roses are {color}")
+print("{plural nown} are Blue")
+print("I love {celebrity}")
