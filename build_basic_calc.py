c152c7d08ce3309c8d3e32a904102894bff662cf
Samih Abdeljabbar <49135655+samih-adam@users.noreply.github.com>
2021-10-19 13:50:46 -0500

    end of calc
    Solved by using floats function instead of int number

--- build_basic_calc.py
+++ build_basic_calc.py
@@ -2,7 +2,11 @@
 
 num1 = input("Enter a number: ")
 num2 = input("Enter another number: ")
-result = num1 + num2 
+result = float(num1) + float(num2)
+
 print(result)
 
 # so not quite the answer we were looking for lol
+#you can use the int function to turn the num variable into an interger!
+#int function is flawed though because it only looks for whole numbers
+#you can than use the float function to parlay that. Float can do whole numbers or decimals(floats)
