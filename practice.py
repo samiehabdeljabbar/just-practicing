d5de53b4f0a040921781764e9c583af2d04215ed
Samieh Jarabba <abdels6375@gmail.com>
2021-10-18 19:56:03 -0500

    first lesson

--- practice.py
+++ practice.py
@@ -1,2 +1,5 @@
 # Beginner python course
 
+# first lesson
+# we are going to be using the print function
+# the print function does exactly what it says, it prints out whatever you put in to that function 
