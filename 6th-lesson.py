00cac862ff44fcb27dbeaeb4bec2f7949ca76998
Samih Abdeljabbar <49135655+samih-adam@users.noreply.github.com>
2021-10-19 13:28:28 -0500

    learning inputs

---
+++ 6th-lesson.py
@@ -0,0 +1,6 @@
+#getting input from users
+#all you have to do is type in input and they can add info
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Hello " + name + "! You are " + age + ", Thats Awesome!")
