0325eece9f961300597300d969a08f216c5beb18
Samih Abdeljabbar <49135655+samih-adam@users.noreply.github.com>
2021-10-19 13:07:33 -0500

    update

--- fifth-lesson.py
+++ fifth-lesson.py
@@ -1,4 +1,7 @@
 #working with numbers!
+#this underneath will allow us to work with more math functions 
+from math import *
+
 
 print(2)
 print(-2.3467)
@@ -9,3 +12,31 @@ print(3+17.26)
 
 #more basic math
 print( 3 * 7 + 14.6)
+
+print(3 * (4 +5))
+
+print(10 % 3) #prints out the remainder of what this number would be
+
+#you can also store these numbers in variables!
+
+my_Num = -5
+print(my_Num)
+#you can also print it out in a string
+print(str(my_Num))
+print(str(my_Num) + " is my favorite number")
+
+#printing out the absoulute value of any number
+print(abs(my_Num))
+
+#printing out  to the power of, first number is the number, the second number is to the power of
+print(pow(3,10))
+
+#printing out max, will print out the higher number
+print(max(4,600))
+
+#doing teh same with min will print out the lower number
+print(min(4,600))
+
+#printing with the round function, will allow you to round up to the nearest number!
+print(round(3.776))
+print(round(3.3))
