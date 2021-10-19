f84ed3917a39ee673e05a769c28a2de12d137df7
Samih Abdeljabbar <49135655+samih-adam@users.noreply.github.com>
2021-10-19 15:26:12 -0500

    doing it his way

--- mad_lib2.py
+++ mad_lib2.py
@@ -1,5 +1,9 @@
 # I first tried it out my way, now I am seeing how the instructor will make the mad libs work!
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+print("Roses are " + color)
+print(plural_noun +" are Blue")
+print("I love " + celebrity)
 
-print("Roses are {color}")
-print("{plural nown} are Blue")
-print("I love {celebrity}")
+
