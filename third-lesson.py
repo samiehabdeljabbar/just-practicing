c8e8aabdb3049926d5b714e05fc52d9c83323c74
Samih Abdeljabbar <49135655+samih-adam@users.noreply.github.com>
2021-10-19 10:12:24 -0500

    learning about different types of variables

--- third-lesson.py
+++ third-lesson.py
@@ -2,11 +2,21 @@
 #Why manually change each line, rather we can use variables to change multiple lines of data at once
 #Imagine doing that thousands of time in ten's of thousands of code
 
+#these are strings that I am storing value in. a string is where you store plain text
+#another store of value I can use is numbers
+#another data that I can store is boolean values, which are true or false statements
+
+
+is_male = False
 character_name = "John"
-character_age = "23"
+#Example of a number valuable below
+character_age = 50
 
 
-print("There once was a man named Samieh,")
-print("He was 70 years old.")
-print("He Really liked the name Samieh,")
-print("but didnt like being 70.")
+print("There once was a man named " + character_name + ",")
+print("He was " + character_age + " years old.")
+
+character_name = "Mike"
+
+print("He Really liked the name " + character_name + ",")
+print("but didnt like being " + character_age+".")
