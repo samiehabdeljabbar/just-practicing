3431686d894bdeae3a7da99f0e1b53857923ad62
Samih Abdeljabbar <49135655+samih-adam@users.noreply.github.com>
2021-10-19 15:30:25 -0500

    updating the mad lib to add my own

--- mad_lib2.py
+++ mad_lib2.py
@@ -2,8 +2,10 @@
 color = input("Enter a color: ")
 plural_noun = input("Enter a plural noun: ")
 celebrity = input("Enter a celebrity: ")
+verb = input("Enter a verb: ")
 print("Roses are " + color)
 print(plural_noun +" are Blue")
 print("I love " + celebrity)
+print("I hate to " + verb)
 
 
