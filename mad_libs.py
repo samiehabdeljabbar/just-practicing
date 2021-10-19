7d225c4dd514063b863623bdfe769f15a39b3958
Samih Abdeljabbar <49135655+samih-adam@users.noreply.github.com>
2021-10-19 14:48:00 -0500

    trying out by myself

--- mad_libs.py
+++ mad_libs.py
@@ -1,5 +1,8 @@
 #crating a basic mad libs game
+adjective = input("Roses are ")
+adjective2 = input("Violets are ")
+verb1 = input("I " + " Love")
+print("Roses are " + adjective)
+print("Violets are " + adjective2)
+print("I " + verb1 + " you")
 
-print("Roses are red")
-print("Violets are blue")
-print("I love you")
