a589fb657d576ff6f47d22a85a65267125804919
Samih Abdeljabbar <49135655+samih-adam@users.noreply.github.com>
2021-10-19 10:32:07 -0500

    working with strings

---
+++ fourth-lesson.py
@@ -0,0 +1,26 @@
+#working with strings
+
+#in order to use a string I need to use quotation marks
+
+# \n makes things print virtically 
+print("Giraffe\nAcademy")
+
+# \# basically means stop python, add this quotation than continue printing this statement
+print("Giraffe\"Academy\"")
+
+phrase = "Giraffe Academy"
+print(phrase)
+
+#concantination
+
+print(phrase + " is cool")
+
+#printing the string all in lower case
+
+print(phrase.lower())
+
+#printing in all uppercase
+
+print(phrase.upper())
+
+print(phrase.isupper())
