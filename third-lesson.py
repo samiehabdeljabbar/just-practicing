594242a7fd50e76513c21f5f623ace23659bb449
Samih Abdeljabbar <49135655+samih-adam@users.noreply.github.com>
2021-10-19 09:55:43 -0500

    Third lesson variables

--- third-lesson.py
+++ third-lesson.py
@@ -1,9 +1,12 @@
-print("There once was a man named Samieh,")
-print("He was 70 years old.")
-print("He Really liked the name Samieh,")
-print("but didnt like being 70.")
-
 #What we are going to do is practice variables!
 #Why manually change each line, rather we can use variables to change multiple lines of data at once
 #Imagine doing that thousands of time in ten's of thousands of code
 
+character_name = "John"
+character_age = "23"
+
+
+print("There once was a man named Samieh,")
+print("He was 70 years old.")
+print("He Really liked the name Samieh,")
+print("but didnt like being 70.")
