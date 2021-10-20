0d96333f66c2282773530a5e1a0823d9ecf24920
Samih Abdeljabbar <49135655+samih-adam@users.noreply.github.com>
2021-10-20 15:36:23 -0500

    list

--- lists.py
+++ lists.py
@@ -2,3 +2,9 @@
 #usually in python you will be working with large amounts of data
 
 friends = ["Kevin", "Karen", "Jim"]
+friends222 = ["Kevin", 2, False]
+
+print(friends)
+print(friends[0])
+print(friends[2])
+print(friends[1])
