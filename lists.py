3e99ee054f4fe53ff018bdab212780ea45bc4cc8
Samih Abdeljabbar <49135655+samih-adam@users.noreply.github.com>
2021-10-20 15:41:15 -0500

    new list items

--- lists.py
+++ lists.py
@@ -8,3 +8,14 @@ print(friends)
 print(friends[0])
 print(friends[2])
 print(friends[1])
+#can also index list backwards with negatives
+print(friends[-1])
+print(friends[-2])
+#can also select more 
+print(friends[1:])
+#you can also pull from a range of people or list items as well
+friend = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+print(friend[1:3])
+print(friend[1:4])
+friend[1] = "Mike"
+print(friend) 
