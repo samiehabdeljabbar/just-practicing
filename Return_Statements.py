4945f49b587ec1e91b7e1202329313a4b67ce334
Samih Abdeljabbar <49135655+samih-adam@users.noreply.github.com>
2021-10-21 14:58:25 -0500

    creating a return statment

--- Return_Statements.py
+++ Return_Statements.py
@@ -1,1 +1,16 @@
 #working wiht Return Statements
+#The Python return statement instructs Python to continue executing a main program. 
+# You can use the return statement to send a value back to the main program, 
+# such as a string or a list
+
+
+def cube(num):
+    return num*num*num
+
+print(cube(7))
+
+def cube2(num):
+    return num*num*num
+
+print(cube2(4))
+
