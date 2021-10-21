31700510f565b3f83cd8144b7b093aa9cd17fc15
Samih Abdeljabbar <49135655+samih-adam@users.noreply.github.com>
2021-10-21 10:08:08 -0500

    update

--- functions.py
+++ functions.py
@@ -8,3 +8,10 @@ print("Top")
 #calling the function by typing the function anme
 say_hi()
 print("Bottom")
+
+def take_input():
+    name = input("What is your name ")
+    age = input("What is your age ")
+    print("Hello " + name + "! You are " + age + ", Thats Awesome!")
+
+take_input()
