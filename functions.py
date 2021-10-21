5f9e0d41b70532ab1a7429e5b6035285ff33bee1
Samih Abdeljabbar <49135655+samih-adam@users.noreply.github.com>
2021-10-21 10:04:26 -0500

    functions

---
+++ functions.py
@@ -0,0 +1,10 @@
+#learning functions 
+
+#key work for functions is def
+
+def say_hi():
+    print("Hello User")
+print("Top")
+#calling the function by typing the function anme
+say_hi()
+print("Bottom")
