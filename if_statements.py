b7f34e328d358fbd57e9e251f08c576a5cb6c3e1
Samieh Jarabba <abdels6375@gmail.com>
2021-10-23 22:38:18 -0500

    update

--- if_statements.py
+++ if_statements.py
@@ -1,9 +1,10 @@
 #working on if statements
 
 #is_male = True 
-is_male = False
+is_male = True
+is_tall = True
 
-if  is_male:
-    print("You are a male")
+if  is_male or is_tall:
+    print("You are a male or tall or both")
 else:
-    print("You are not a male")
+    print("You are neither male or tall")
