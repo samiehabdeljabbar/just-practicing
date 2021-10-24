4e2464a92d85a384ae738f6a3d915264a8e94c78
Samieh Jarabba <abdels6375@gmail.com>
2021-10-24 09:54:35 -0500

    trying more statements out

--- if_statements.py
+++ if_statements.py
@@ -1,11 +1,14 @@
 #working on if statements
 
 #is_male = True 
-is_male = True
+is_male = False
 is_tall = False
 #you can use these two phrases (and, or)
 if  is_male and is_tall:
     print("You are a tall male")
-elif is_male and not (is_tall)
+elif is_male and not (is_tall):
+    print("You are a short make")
+elif not (is_male) and is_tall:
+    print("you are not a male but are tall")
 else:
     print("You are neither male or not tall or both")
